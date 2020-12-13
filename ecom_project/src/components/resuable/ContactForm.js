4fc502469fea860caa581d8bb38b4f5eab976842
Akhilesh Sharma <akhileshsharma601@yahoo.com>
2020-12-13 23:48:35 +0530

    contact page added

---
+++ ecom_project/src/components/resuable/ContactForm.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+export default function ContactForm() {
+  return (
+    <section classNameName="py-3">
+      <div className="col-10 col-sm-8 mx-auto">
+        <form action="https://formspree.io/f/{akhileshsharma601@yahoo.com}" method="POST">
+          <div className="form-group">
+            <input
+              type="text"
+              name="Name"
+              id="name"
+              placeholder="Your Name Here"
+              className="form-control mb-2"
+            />
+            <input
+              type="email"
+              name="Email"
+              id="email"
+              placeholder="Your Email Here"
+              className="form-control mb-2"
+            />
+            <input
+              type="Mobile"
+              name="Mobile"
+              id="mobile"
+              placeholder="Your Mobile Number Here"
+              className="form-control mb-2"
+            />
+             <textarea
+              type="description"
+              name="description"
+              id="description"
+              placeholder="Your Message Here"
+              className="form-control mb-2"
+            />
+            <button type="submit" className="btn btn-outline-info btn-block">Submit</button>
+          </div>
+        </form>
+      </div>
+    </section>
+  )
+}
--- ecom_project/src/pages/contact.js
+++ ecom_project/src/pages/contact.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react"
+import { graphql } from "gatsby"
+import HeroSection from "../components/resuable/HeroSection"
+import Infoblock from '../components/resuable/Infoblock'
+//import DualInfo from '../components/resuable/DualInfo'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import ContactForm from '../components/resuable/ContactForm'
 
-export default function contact() {
-    return (
-        <div>
-            hello from contact page
-        </div>
-    )
-}
+const ContactPage = ({ data }) => (
+  <Layout>
+    <SEO title="Home" />
+    <HeroSection
+      img={data.img.childImageSharp.fluid}
+      title="Contact us"
+      subtitle=""
+      heroclass="about-background"
+    />
+   
+    <Infoblock heading="How can we help ?"/>
+   <ContactForm/>
+    
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "contact.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
+
+export default ContactPage
